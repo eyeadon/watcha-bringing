ab48b4e286114858dad9498d4ebda6df46cd8450
eyeadon <eyeadon40@gmail.com>
2024-07-17 20:33:53 -0400

    plus and minus buttons for add dish / add bev

--- src/components/ExpandableSection.tsx
+++ src/components/ExpandableSection.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, useState } from "react";
+import { PlusLg, DashLg } from "react-bootstrap-icons";
 
 interface Props {
   children: ReactNode;
@@ -10,15 +11,23 @@ const ExpandableSection = ({ children, buttonLabelText }: Props) => {
 
   const showDiv = isExpanded ? children : null;
 
+  const renderIcon = isExpanded ? (
+    <DashLg color="white" />
+  ) : (
+    // was making whole thing a string by + buttonLabelText
+    [<PlusLg color="white" />, " ", buttonLabelText]
+  );
+
   return (
     <>
       <div className="mb-3">
         <button
+          id="myButton"
           type="button"
           className="btn btn-primary"
           onClick={() => setExpanded(!isExpanded)}
         >
-          {isExpanded ? "-" : "+ " + buttonLabelText}
+          {renderIcon}
         </button>
       </div>
       {showDiv}
