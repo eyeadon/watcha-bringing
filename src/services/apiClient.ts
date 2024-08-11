3a220fc46a3ac32c6e0d9df14a43ab3613b2376c
eyeadon <eyeadon40@gmail.com>
2024-08-11 17:00:23 -0400

    added axios and api client file

---
+++ src/services/apiClient.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "",
+  params: {
+    key: "",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  // *this* issue with callbacks, could do the following in constructor
+  // if it's a regular function:
+  // this.getAll = this.getAll.bind(this);
+
+  //  returns response object
+  // arrow functions do not have their own *this* context
+  getAll = (config: AxiosRequestConfig) => {
+    return (
+      axiosInstance
+        // config is for passing config object of query string params to back end
+        // config is optional in get
+        .get<FetchResponse<T>>(this.endpoint, config)
+        .then((res) => res.data)
+    );
+  };
+
+  // get single object
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
+
+  post = (data: T) => {
+    return axiosInstance
+      .post<FetchResponse<T>>(this.endpoint, data)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
