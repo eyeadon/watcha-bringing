5335dfb08f30ebdd4eb41913a0ba4fc0506ffe65
eyeadon <eyeadon40@gmail.com>
2024-08-17 16:56:41 -0400

    added routes for dishes and bevs

---
+++ routes/bevs.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const { Bev, validate } = require("../models/bev");
+
+// get all
+router.get("/", async (req, res) => {
+  // sort by what order, name or category?
+  const bevs = await Bev.find().sort("category");
+  res.send(bevs);
+});
+
+// get single
+router.get("/:id", async (req, res) => {
+  const bev = await Bev.findById(req.params.id);
+
+  if (!bev)
+    return res.status(404).send("The bev with the given ID was not found.");
+  res.send(bev);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const bev = new Bev({
+    category: req.body.category,
+    name: req.body.name,
+    amount: req.body.amount,
+  });
+
+  try {
+    // returns Promise, not stored here
+    await bev.save();
+  } catch (exception) {
+    for (field in exception.errors) {
+      console.log(exception.errors[field].message);
+    }
+  }
+
+  res.send(bev);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const bev = await Bev.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        category: req.body.category,
+        name: req.body.name,
+        amount: req.body.amount,
+      },
+    },
+    // get updated document
+    { new: true }
+  );
+
+  if (!bev)
+    return res.status(404).send("The bev with the given ID was not found.");
+
+  res.send(bev);
+});
+
+router.delete("/:id", async (req, res) => {
+  const bev = await Bev.findByIdAndDelete(req.params.id);
+
+  if (!bev)
+    return res.status(404).send("The bev with the given ID was not found.");
+
+  res.send(bev);
+});
+
+module.exports = router;
---
+++ routes/dishes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const { Dish, validate } = require("../models/dish");
+
+// get all
+router.get("/", async (req, res) => {
+  // sort by what order, name or category?
+  const dishes = await Dish.find().sort("category");
+  res.send(dishes);
+});
+
+// get single
+router.get("/:id", async (req, res) => {
+  const dish = await Dish.findById(req.params.id);
+
+  if (!dish)
+    return res.status(404).send("The dish with the given ID was not found.");
+  res.send(dish);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const dish = new Dish({
+    category: req.body.category,
+    name: req.body.name,
+    amount: req.body.amount,
+    dietary: req.body.dietary,
+  });
+
+  try {
+    // returns Promise, not stored here
+    await dish.save();
+  } catch (exception) {
+    for (field in exception.errors) {
+      console.log(exception.errors[field].message);
+    }
+  }
+
+  res.send(dish);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const dish = await Dish.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        category: req.body.category,
+        name: req.body.name,
+        amount: req.body.amount,
+        dietary: req.body.dietary,
+      },
+    },
+    // get updated document
+    { new: true }
+  );
+
+  if (!dish)
+    return res.status(404).send("The dish with the given ID was not found.");
+
+  res.send(dish);
+});
+
+router.delete("/:id", async (req, res) => {
+  const dish = await Dish.findByIdAndDelete(req.params.id);
+
+  if (!dish)
+    return res.status(404).send("The dish with the given ID was not found.");
+
+  res.send(dish);
+});
+
+module.exports = router;
