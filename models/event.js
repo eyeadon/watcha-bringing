cda32b0178bc34b645c7439d7e630b9bf4334728
eyeadon <eyeadon40@gmail.com>
2024-09-17 21:42:32 -0400

    working on event model

---
+++ models/event.js
@@ -0,0 +1,87 @@
+import Joi from "joi";
+import mongoose from "mongoose";
+
+const addressSchema = new Schema({
+  street: String,
+  city: String,
+  state: {
+    type: String,
+    minlength: 2,
+    maxlength: 2,
+    uppercase: true,
+    trim: true,
+  },
+  zipcode: {
+    type: String,
+    minlength: 5,
+    maxlength: 5,
+    trim: true,
+  },
+});
+
+// uses default connection
+const Event = mongoose.model(
+  "Event",
+  new mongoose.Schema({
+    publicId: { type: String, required: true },
+    category: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      lowercase: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      lowercase: true,
+      trim: true,
+    },
+    host: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      lowercase: true,
+      trim: true,
+    },
+    address: {
+      type: addressSchema,
+      required: false,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: false,
+    },
+  })
+);
+
+function validateEvent(event) {
+  const schema = Joi.object({
+    publicId: Joi.string().pattern(/^[A-Za-z0-9_-]{21}$/, "nanoid"),
+    category: Joi.string().min(3).max(50).required(),
+    name: Joi.string().min(3).max(50).required(),
+    host: Joi.string().min(3).max(50).required(),
+    address: Joi.object({}),
+    date: Joi.date(),
+    startTime: Joi.string(),
+    endTime: Joi.string(),
+  });
+
+  return schema.validate(event);
+}
+
+// exports.Event = Event;
+// exports.validate = validateEvent;
+export { Event, validateEvent };
