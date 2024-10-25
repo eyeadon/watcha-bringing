e09bfda6df14ccbaa262d873e35bb117f5ff2383
eyeadon <eyeadon40@gmail.com>
2024-10-25 18:35:19 -0400

    displaying date with format August 16, 2018

--- src/components/SelectedEventDataDisplay.tsx
+++ src/components/SelectedEventDataDisplay.tsx
@@ -1,18 +1,22 @@
 import { Event } from "../interfaces/interfaces";
 import { capitalizeFirstLetter } from "../functions/functions";
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 
 interface Props {
   selectedEvent: Event;
 }
 
 const SelectedEventTitle = ({ selectedEvent }: Props) => {
+  dayjs.extend(localizedFormat);
+
   return (
     <>
       {selectedEvent.publicId !== "none" && (
         <div className="d-flex p-2 mb-3">
           <div className="p-4">
             <h4>Host:&nbsp;{capitalizeFirstLetter(selectedEvent.host)}</h4>
-            <h4>Date:&nbsp;{capitalizeFirstLetter(selectedEvent.date)}</h4>
+            <h4>Date:&nbsp;{dayjs(selectedEvent.date).format("LL")}</h4>
             <h4>
               Start Time:&nbsp;{capitalizeFirstLetter(selectedEvent.startTime)}
             </h4>
