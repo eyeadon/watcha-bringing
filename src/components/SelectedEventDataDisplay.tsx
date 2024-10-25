c88d6f15adf1d6378985ebce99bf12f4e2516bdc
eyeadon <eyeadon40@gmail.com>
2024-10-25 18:16:55 -0400

    display event data only if there is a valid selectedEvent

--- src/components/SelectedEventDataDisplay.tsx
+++ src/components/SelectedEventDataDisplay.tsx
@@ -7,34 +7,38 @@ interface Props {
 
 const SelectedEventTitle = ({ selectedEvent }: Props) => {
   return (
-    <div className="d-flex p-2 mb-3">
-      <div className="p-4">
-        <h4>Host:&nbsp;{capitalizeFirstLetter(selectedEvent.host)}</h4>
-        <h4>Date:&nbsp;{capitalizeFirstLetter(selectedEvent.date)}</h4>
-        <h4>
-          Start Time:&nbsp;{capitalizeFirstLetter(selectedEvent.startTime)}
-        </h4>
-        <h4>
-          End Time:&nbsp;
-          {selectedEvent.endTime
-            ? capitalizeFirstLetter(selectedEvent.endTime)
-            : "?"}
-        </h4>
-      </div>
-      <div className="p-4">
-        <h4>Address:&nbsp;</h4>
-        <p>{capitalizeFirstLetter(selectedEvent.address.street)}</p>
-        <p>
-          {selectedEvent.publicId !== "none" &&
-            capitalizeFirstLetter(selectedEvent.address.city) +
-              ", " +
-              " " +
-              capitalizeFirstLetter(selectedEvent.address.state) +
-              " " +
-              capitalizeFirstLetter(selectedEvent.address.zipcode)}
-        </p>
-      </div>
-    </div>
+    <>
+      {selectedEvent.publicId !== "none" && (
+        <div className="d-flex p-2 mb-3">
+          <div className="p-4">
+            <h4>Host:&nbsp;{capitalizeFirstLetter(selectedEvent.host)}</h4>
+            <h4>Date:&nbsp;{capitalizeFirstLetter(selectedEvent.date)}</h4>
+            <h4>
+              Start Time:&nbsp;{capitalizeFirstLetter(selectedEvent.startTime)}
+            </h4>
+            <h4>
+              End Time:&nbsp;
+              {selectedEvent.endTime
+                ? capitalizeFirstLetter(selectedEvent.endTime)
+                : "?"}
+            </h4>
+          </div>
+
+          <div className="p-4">
+            <h4>Address:&nbsp;</h4>
+            <p>{capitalizeFirstLetter(selectedEvent.address.street)}</p>
+            <p>
+              {capitalizeFirstLetter(selectedEvent.address.city) +
+                ", " +
+                " " +
+                capitalizeFirstLetter(selectedEvent.address.state) +
+                " " +
+                capitalizeFirstLetter(selectedEvent.address.zipcode)}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
