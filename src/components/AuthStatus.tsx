fcea70dffa281ec34520ba550b4c20da54bc7b9a
eyeadon <eyeadon40@gmail.com>
2025-03-03 17:13:03 -0500

    notes about user actions. renamed user to auth0user

--- src/components/AuthStatus.tsx
+++ src/components/AuthStatus.tsx
@@ -3,17 +3,26 @@ import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
 const AuthStatus = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  // access auth state
+  const { user: auth0user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) return <div>Loading...</div>;
 
   if (isAuthenticated) {
-    // check if new user
+    // so we can add our own user data, events/dishes owned:
+    // check if user exists in App database, get
+    // in server route: const user = await User.findById(req.params.id);
+    // if user not found -> if (error), create new user, post, return user
+    // if user found -> return user
+    // add event to user.eventsOwned
+    // add dish to user.dishesOwned
+
+    // can we just use put?
 
     return (
       // TODO fix styles
       <div className="">
-        <h4 style={{ color: "black" }}>{user!.name}</h4>
+        <h4 style={{ color: "black" }}>{auth0user!.name}</h4>
         <LogoutButton />
       </div>
     );
