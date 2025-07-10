20721916e9a530f43773d85e3cf7b6f1fb26b257
eyeadon <eyeadon40@gmail.com>
2025-07-10 16:03:51 -0400

    log in button, background color and bold text

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,10 +1,18 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+  const BoldButton = styled(Button)({
+    fontWeight: "bold",
+    "&:hover": {
+      backgroundColor: "#2d363c",
+    },
+  });
+
+  return <BoldButton onClick={() => loginWithRedirect()}>Log In</BoldButton>;
 };
 
 export default LoginButton;
