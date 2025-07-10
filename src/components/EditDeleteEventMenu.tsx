d812c098e9cb496794a5632995986a38e1cf163f
eyeadon <eyeadon40@gmail.com>
2025-07-10 16:06:14 -0400

    import statement cleanup

--- src/components/EditDeleteEventMenu.tsx
+++ src/components/EditDeleteEventMenu.tsx
@@ -5,7 +5,6 @@ import { SelectedEventContext } from "../contexts/contexts";
 import useDeleteEvent from "../hooks/useDeleteEvent";
 import { EventDocumentType } from "../interfaces/interfaces";
 import { useAuth0 } from "@auth0/auth0-react";
-import useUser from "../hooks/useUser";
 import { isOwned } from "../functions/functions";
 import useUserByEmail from "../hooks/useUserByEmail";
 
